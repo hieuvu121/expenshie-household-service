fd05c1d04e42d5742fe2a89f58bb390107d4785e
hieuvu121 <hieubeobilabi@gmail.com>
2026-05-06 17:27:03 +1000

    Initial project structure: household-service

---
+++ src/main/java/com/be9expensphie/household/HouseholdServiceApplication.java
@@ -0,0 +1,11 @@
+package com.be9expensphie.household;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HouseholdServiceApplication.class, args);
+    }
+}
